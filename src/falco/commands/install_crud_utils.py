2d298b58b3bc2f2cc083d1c1f8542dda9b39bc1c
Tobi DEGNON <tobidegnon@proton.me>
2023-12-24 16:34:30 +0100

    feat: crud utils should be installed apps_dir

--- src/falco/commands/install_crud_utils.py
+++ src/falco/commands/install_crud_utils.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Annotated
 
 import cappa
+from falco.utils import get_current_dir_as_project_name
 from falco.utils import get_falco_blueprints_path
 from falco.utils import simple_progress
 from rich import print as rich_print
@@ -16,6 +17,10 @@ DEFAULT_INSTALL_PATH = "core/utils.py"
 
 @cappa.command(help="Install utils necessary for CRUD views.", name="install-crud-utils")
 class InstallCrudUtils:
+    apps_dir: Annotated[
+        Path | None,
+        cappa.Arg(default=None, help="The path to your django apps directory."),
+    ]
     output: Annotated[
         Path,
         cappa.Arg(
@@ -26,12 +31,18 @@ class InstallCrudUtils:
         ),
     ]
 
-    def __call__(self):
+    def __call__(self, project_name: Annotated[str, cappa.Dep(get_current_dir_as_project_name)]):
+        if not self.apps_dir:
+            self.apps_dir = Path() / project_name
+
+        output = self.apps_dir / self.output
+
         with simple_progress("Installing crud utils"):
             imports_template, code_template = extract_python_file_templates(UTILS_FILE.read_text())
-            output_file = self.output if self.output.name.endswith(".py") else self.output / "utils.py"
+            output_file = output if output.name.endswith(".py") else output / "utils.py"
             output_file.parent.mkdir(parents=True, exist_ok=True)
+            (output_file.parent / "__init__.py").touch(exist_ok=True)
             output_file.touch(exist_ok=True)
             output_file.write_text(imports_template + output_file.read_text() + code_template)
         run_python_formatters(str(output_file))
-        rich_print("[green]CRUD Utils installed successfully.")
+        rich_print(f"[green]CRUD Utils installed successfully to {output_file}.")
--- tests/commands/test_install_crud_utils.py
+++ tests/commands/test_install_crud_utils.py
@@ -5,17 +5,34 @@ from falco.commands.install_crud_utils import DEFAULT_INSTALL_PATH
 
 
 def test_install_crud_utils(runner: CommandRunner):
-    runner.invoke("install-crud-utils")
+    runner.invoke("install-crud-utils", ".")
     assert Path(DEFAULT_INSTALL_PATH).exists()
+    assert Path("core/__init__.py").exists()
 
 
 def test_install_crud_utils_to_output_dir(runner: CommandRunner):
     output = Path("core")
-    runner.invoke("install-crud-utils", "-o", str(output.resolve()))
+    runner.invoke("install-crud-utils", ".", "-o", str(output.resolve()))
     assert (output / "utils.py").exists
 
 
 def test_install_crud_utils_to_output_file(runner: CommandRunner):
     output = Path("core/utils.py")
-    runner.invoke("install-crud-utils", "-o", str(output.resolve()))
+    runner.invoke("install-crud-utils", ".", "-o", str(output.resolve()))
     assert output.exists()
+
+
+def test_install_crud_utils_in_specific_apps_dir(runner: CommandRunner):
+    apps_dir = Path("apps")
+    runner.invoke("install-crud-utils", str(apps_dir.resolve()))
+    assert (apps_dir / DEFAULT_INSTALL_PATH).exists()
+    assert (apps_dir / "core/__init__.py").exists()
+
+
+def test_install_crud_utils_in_specific_apps_dir_and_output(runner: CommandRunner):
+    apps_dir = Path("apps")
+    output = "core"
+    dest_folder = apps_dir / output
+    runner.invoke("install-crud-utils", str(apps_dir.resolve()), "-o", output)
+    assert (dest_folder / "utils.py").exists()
+    assert (dest_folder / "__init__.py").exists()
