b641ee34f79f400b403497af9100d00d7cebb77d
Tobi DEGNON <tobidegnon@proton.me>
2023-12-24 10:26:10 +0100

    test: crud tests

---
+++ tests/commands/test_crud.py
@@ -0,0 +1,123 @@
+import subprocess
+from pathlib import Path
+
+import cappa
+import pytest
+from cappa.testing import CommandRunner
+
+
+views_functions = ["post_list", "post_detail", "post_update", "post_create"]
+html_templates = [
+    "post_list.html",
+    "post_detail.html",
+    "post_update.html",
+    "post_create.html",
+]
+views_functions_entry_point = ["index", "detail", "update", "create"]
+html_templates_point = [
+    "index.html",
+    "detail.html",
+    "update.html",
+    "create.html",
+]
+forms_attributes = ["PostForm", "Post", "title", "content"]
+
+
+def healthy_django_project() -> bool:
+    result = subprocess.run(
+        ["python", "manage.py", "check"],
+        check=False,
+        capture_output=True,
+        text=True,
+    )
+    print(result.stderr)
+    return result.returncode == 0
+
+
+def register_project_urls():
+    urls_file = Path("myproject/urls.py")
+    urls_file.write_text(
+        "from django.urls import include\n\n"
+        + urls_file.read_text()
+        + "\n\nurlpatterns += [path('blog/', include('blog.urls'))]",
+    )
+
+
+def test_crud(django_project, runner: CommandRunner, set_git_repo_to_clean):
+    runner.invoke("crud", "blog.post")
+    runner.invoke("install-crud-utils")
+    register_project_urls()
+    assert healthy_django_project()
+    app_dir = Path("blog")
+    assert (app_dir / "urls.py").exists()
+    for a in forms_attributes:
+        assert a in (app_dir / "forms.py").read_text()
+
+    # sourcery skip: no-loop-in-tests
+    for f in views_functions:
+        assert f in (app_dir / "views.py").read_text()
+    for t in html_templates:
+        assert (app_dir / "templates" / "blog" / f"{t}").exists()
+
+
+def test_crud_entry_point(django_project, runner: CommandRunner, set_git_repo_to_clean):
+    runner.invoke("crud", "blog.post", "--entry-point")
+    runner.invoke("install-crud-utils")
+    register_project_urls()
+    assert healthy_django_project()
+    app_dir = Path("blog")
+    assert (app_dir / "urls.py").exists()
+    for a in forms_attributes:
+        assert a in (app_dir / "forms.py").read_text()
+    # sourcery skip: no-loop-in-tests
+    for f in views_functions_entry_point:
+        assert f in (app_dir / "views.py").read_text()
+    for t in html_templates_point:
+        assert (app_dir / "templates" / "blog" / f"{t}").exists()
+
+
+def test_crud_only_html(django_project, runner: CommandRunner, set_git_repo_to_clean):
+    runner.invoke("crud", "blog.post", "--only-html")
+    runner.invoke("install-crud-utils")
+    assert healthy_django_project()
+    app_dir = Path("blog")
+    assert not (app_dir / "urls.py").exists()
+    assert not (app_dir / "forms.py").exists()
+    # sourcery skip: no-loop-in-tests
+    for f in views_functions:
+        assert f not in (app_dir / "views.py").read_text()
+    for t in html_templates:
+        assert (app_dir / "templates" / "blog" / f"{t}").exists()
+
+
+def test_crud_only_python(django_project, runner: CommandRunner, set_git_repo_to_clean):
+    runner.invoke("crud", "blog.post", "--only-python")
+    runner.invoke("install-crud-utils")
+    register_project_urls()
+    assert healthy_django_project()
+    app_dir = Path("blog")
+    assert (app_dir / "urls.py").exists()
+    # sourcery skip: no-loop-in-tests
+    for a in forms_attributes:
+        assert a in (app_dir / "forms.py").read_text()
+    # sourcery skip: no-loop-in-tests
+    for f in views_functions:
+        assert f in (app_dir / "views.py").read_text()
+    for t in html_templates:
+        assert not (app_dir / "templates" / "blog" / f"{t}").exists()
+
+
+def test_crud_repo_not_clean(django_project, runner: CommandRunner):
+    with pytest.raises(cappa.Exit):
+        runner.invoke("crud", "blog.post")
+
+
+def test_crud_exclude_field(django_project, runner: CommandRunner, set_git_repo_to_clean):
+    runner.invoke("crud", "blog.post", "--only-python", "-e=title")
+    runner.invoke("install-crud-utils")
+    app_dir = Path("blog")
+    # sourcery skip: no-loop-in-tests
+    assert "title" not in (app_dir / "forms.py").read_text()
+    forms_attributes_ = ["PostForm", "Post", "content"]
+    for a in forms_attributes_:
+        assert a in (app_dir / "forms.py").read_text()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
 import pytest
 from cappa.testing import CommandRunner
@@ -19,21 +21,47 @@ def runner():
 @pytest.fixture
 def django_project(tmp_path):
     project_dir = tmp_path / "myproject"
-    subprocess.run(["django-admin", "startproject", "myproject", str(project_dir)], check=True)
+    subprocess.run(["django-admin", "startproject", "myproject"], check=True)
     os.chdir(project_dir)
 
     # Create a new Django app
-    subprocess.run(["python", "manage.py", "startapp", "myapp"], check=True)
+    subprocess.run(["python", "manage.py", "startapp", "blog"], check=True)
 
     # Create a basic model in the app
     model_code = """
 from django.db import models
 
-class MyModel(models.Model):
-    field1 = models.CharField(max_length=200)
-    field2 = models.IntegerField()
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
     """
-    (project_dir / "myapp" / "models.py").write_text(model_code)
+    (project_dir / "blog" / "models.py").write_text(model_code)
+
+    # Register the app in the project's settings
+    settings_file = project_dir / "myproject" / "settings.py"
+    settings_content = settings_file.read_text()
+    settings_file.write_text(settings_content + "\n" + "INSTALLED_APPS += ['blog']\n")
 
     yield project_dir
     os.chdir(tmp_path)
+
+
+@pytest.fixture
+def set_git_repo_to_clean():
+    # Define the mock function
+    def mock_run(args, **kwargs):
+        if args == ["git", "status", "--porcelain"]:
+            mock = MagicMock()
+            mock.returncode = 0
+            mock.stdout = ""
+            return mock
+        else:
+            # Call the original subprocess.run function
+            return original_run(args, **kwargs)
+
+    # Save the original subprocess.run function
+    original_run = subprocess.run
+
+    # Use patch to replace subprocess.run with the mock function
+    with patch("subprocess.run", side_effect=mock_run):
+        yield
