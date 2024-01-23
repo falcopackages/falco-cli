a47005f4c16375acdc28360555fbfbd62207a72d
Tobi DEGNON <tobidegnon@proton.me>
2024-01-23 14:01:44 +0100

    fix: regression

--- src/falco/commands/checks.py
+++ src/falco/commands/checks.py
@@ -7,7 +7,7 @@ import cappa
 def clean_git_repo(ignore_dirty: bool = False) -> None:
     with suppress(subprocess.CalledProcessError):
         result = subprocess.run(["git", "status", "--porcelain"], capture_output=True, text=True, check=True)
-        if result.stdout.strip() == "":
+        if result.stdout.strip() == "" or ignore_dirty:
             return
     raise cappa.Exit(
         "Your git repo is not clean. Please commit or stash your changes before running this command.",
