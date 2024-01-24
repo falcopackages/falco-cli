405e407d610978674a6225ca2ee0439c9539569c
Tobi DEGNON <tobidegnon@proton.me>
2024-01-24 19:29:40 +0100

    fix: avoid updating multiple urlspattern variable
    if the urlpatterns is defined more than once in the file, it should only update the fist one it encounters

--- src/falco/commands/model_crud.py
+++ src/falco/commands/model_crud.py
@@ -181,6 +181,7 @@ def register_app_urls(app_label: str, app_name: str) -> Path:
                 new_content = "from django.urls import include\n" + new_content
 
                 rool_url_path.write_text(new_content)
+                break
         except AttributeError:
             continue
 
