c1ec156ba2bc1610d6ee677017a156909b266062
Tobi DEGNON <tobidegnon@proton.me>
2024-01-31 16:37:14 +0100

    fix: git not installed and skip_git_check still raise an error

--- src/falco/commands/checks.py
+++ src/falco/commands/checks.py
@@ -5,9 +5,11 @@ import cappa
 
 
 def clean_git_repo(*, ignore_dirty: bool = False) -> None:
+    if ignore_dirty:
+        return 
     with suppress(subprocess.CalledProcessError):
         result = subprocess.run(["git", "status", "--porcelain"], capture_output=True, text=True, check=True)
-        if result.stdout.strip() == "" or ignore_dirty:
+        if result.stdout.strip() == "":
             return
     raise cappa.Exit(
         "Your git repo is not clean. Please commit or stash your changes before running this command.",
