c08512584bc18fd230931e0e0bb3d8c0ad6ac950
Tobi DEGNON <tobidegnon@proton.me>
2024-12-19 07:24:36 +0100

    I don't use this command anymore
    currently I'm not even sure how it should work

--- src/falco_cli/__main__.py
+++ src/falco_cli/__main__.py
@@ -2,7 +2,6 @@ import cappa
 
 from .fmt import Fmt
 from .start_project import StartProject
-from .sync_dotenv import SyncDotenv
 from .update_project import UpdateProject
 
 
@@ -10,7 +9,7 @@ from .update_project import UpdateProject
     help="Enhance your Django developer experience: CLI and Guides for the Modern Django Developer.",
 )
 class Falco:
-    subcommand: cappa.Subcommands[StartProject | UpdateProject | Fmt | SyncDotenv]
+    subcommand: cappa.Subcommands[StartProject | UpdateProject | Fmt]
 
 
 def main():
